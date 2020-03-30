f54285698dafe74c4c6e3b0e9eca27ea5481f3ae
Izabela Iskra <ia007@interia.pl>
2020-03-30 18:22:16 +0200

    1901

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class TrelloBoardDto {
     @JsonProperty("id")
     private String id;
